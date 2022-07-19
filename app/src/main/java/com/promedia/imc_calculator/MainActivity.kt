509b06ac7a384431908522a99dca349eb4e5e9e0
Alumno <dfsrfe>
2022-07-19 20:09:11 +0200

    Más código sin acabar metido (NO funciona)

--- app/src/main/java/com/promedia/imc_calculator/MainActivity.kt
+++ app/src/main/java/com/promedia/imc_calculator/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.p
 
+import android.graphics.Color
 import android.widget.SeekBar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
+import com.promedia.imc_calculator.R
 import com.promedia.imc_calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -53,6 +59,28 @@ class MainActivity : AppCompatActivity() {
             .div(100.0)
         b.tvResultado = IMC.toString()
         calcObesidad()
+    }
+    fun calcObesidad() {
+
+
+    }
+
+    fun mySnackBarCustom (view: View){
+        val s = Snackbar.make(view, "", Snackbar.LENGTH_SHORT)
+        val sbLayout = s.view as Snackbar.SnackbarLayout
+        val customLayout = layoutInflater.inflate(R.layout.toast, null)
+
+        customLayout!!.findViewById<TextView>(R.id.tvToastTitle).text = "VER TABLA"
+
+        sbLayout.addView(customLayout, 0)
+        s.setBackgroundTint(Color.YELLOW)
+        s.show()
+    }
+    fun alertDialog(view:View){
+        AlertDialog.Builder(view.context)
+            .setPositiveButton("Aceptar") { dialog, id -> /* Acción */ }
+            .show()
+
     }
 }
 
